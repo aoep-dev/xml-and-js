b9b459f9caedab85dc2fff1e3f7cc41ba9be717e
Alfred Emasoga Padilla <aoep.dev@gmail.com>
2022-11-18 11:44:01 -0500

    assignment

--- module-08/assignments/assignment.js
+++ module-08/assignments/assignment.js
@@ -2,55 +2,41 @@
  * 1. Replace regular functions with arrow functions
  * 2. Fix callback hell by rewriting it with async/await
  * 3. Make sure the "Finish" is logged after all the data is converted
- */
+*/
+const timeout = (ms = 1000) => new Promise((resolve)=> setTimeout(resolve,ms));
 
-function timeout(ms, callback) {
-  return new Promise(function (resolve) {
-    setTimeout(function () {
-      resolve();
-      callback();
-    }, ms);
-  });
+const generateRandomNumber = () => Math.floor(Math.random() * 40);
+
+const generateData = async() => {
+    await timeout(1000)
+    return Promise.all(Array.from({ length: 20 }, generateRandomNumber));
+
 }
 
-function generateRandomNumber() {
-  return Math.floor(Math.random() * 40);
+const convertToFeet = async (meters) => {
+    const feet = meters * 3.2808;
+    await timeout();
+    logResult(meters, feet)
+
 }
 
-function generateData(callback) {
-  timeout(1000, function () {
-    const data = Array.from({ length: 20 }, generateRandomNumber);
-    callback(data);
-  });
+const processData = async (data) => {
+
+    await Promise.all(data.map(value => convertToFeet(value)))
 }
 
-function convertToFeet(meters, callback) {
-  const feet = meters * 3.2808;
-  timeout(3500, function () {
-    callback(feet);
-  });
+const logResult = (meters, feet) => {
+    console.log(`Converted ${meters}m to ${feet}ft`);
 }
 
-function processData(data, callback) {
-  data.map(function (value) {
-    callback(value);
-  });
-}
+const main = async () => {
 
-function logResult(meters, feet) {
-  console.log(`Converted ${meters}m to ${feet}ft`);
-}
+    console.log("Start");
+    const data = await generateData()
+    await processData(data)
 
-function main() {
-  console.log("Start");
-  generateData(function (data) {
-    processData(data, function (value) {
-      convertToFeet(value, function (result) {
-        logResult(value, result);
-      });
-    });
-  });
-  console.log("Finish");
+    console.log("Finish");
+
 }
 
 main();
